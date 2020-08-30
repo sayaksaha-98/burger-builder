7838d46d71d5b95bd1f795fb4a27c4cf4e61d199
Sayak Saha <sayak.sam98@gmail.com>
2020-08-30 13:27:26 +0530

    sending a post request

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "../../axios-orders";
 
 import Aux from "../../hoc/Aux/Aux";
 import Burger from "../../components/Burger/Burger";
@@ -83,7 +84,24 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You Continue");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Sayak Saha",
+        email: "test@test.com",
+        address: {
+          street: "Test Street 1",
+          pin: "741101",
+          country: "India",
+        },
+      },
+      deliveryMethod: "faastest",
+    };
+    axios
+      .post("/orders.json", order)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
   };
 
   render() {
