6771ca398236782c5bb11e049d39efdc43e897b4
Sayak Saha <sayak.sam98@gmail.com>
2020-09-01 20:29:08 +0530

    getting token from backend

---
+++ src/store/actions/auth.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+import * as actionTypes from "./actionTypes";
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START,
+  };
+};
+
+export const authSuccess = (authData) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    auth: authData,
+  };
+};
+
+export const authFail = (error) => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error,
+  };
+};
+
+export const auth = (email, password) => {
+  return (dispatch) => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    axios
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDoBFxTwPa0rk6NgSMZpOgGZqg9QrV8daQ",
+        authData
+      )
+      .then((response) => {
+        dispatch(authSuccess(response.data));
+      })
+      .catch((err) => {
+        dispatch(authFail(err));
+      });
+  };
+};
