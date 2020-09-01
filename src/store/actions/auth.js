a99cef78af77478ad8ccb2bf3060325450ea01ce
Sayak Saha <sayak.sam98@gmail.com>
2020-09-01 21:01:32 +0530

    storing the token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    auth: authData,
+    idToken: token,
+    userId: userId,
   };
 };
 
@@ -39,7 +40,7 @@ export const auth = (email, password, isSignup) => {
     axios
       .post(url, authData)
       .then((response) => {
-        dispatch(authSuccess(response.data));
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
       })
       .catch((err) => {
         dispatch(authFail(err));
